d8ebb4dc4e0ff0a1f7b760c89181cb88a780d835
Your NamejeffIrvine <jirvine1212@gmail.com>
2021-02-16 21:38:29 -0800

    passing test before refactor

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -38,8 +38,8 @@ function App() {
     <>
       <button onClick={undo}>undo</button>
       <button onClick={redo}>redo</button>
-      <input type="color" value={current} onChange={({ target }) => record(target.value)} />
-      <div style={{ backgroundColor: current, width: '10rem', height: '10rem' }}></div>
+      <input type="color" data-testid="color-input" value={current} onChange={({ target }) => record(target.value)} />
+      <div data-testid="color-display" style={{ backgroundColor: current, width: '10rem', height: '10rem' }}></div>
     </>
   );
 }
--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
   afterEach(() => cleanup());
-  it('renders App', () => {
-    const { asFragment } = render(<App />);
-    expect(asFragment()).toMatchSnapshot();
+  it('tests the changing of state for undo, redo, and record', () => {
+    render(<App />);
+    
+    const undoButton = screen.getByText('undo');
+    const redoButton = screen.getByText('redo');
+    const colorInput = screen.getByTestId('color-input');
+
+    fireEvent.change(colorInput, {
+      target: {
+        value: '#FFFFFF'
+      },
+    });
+
+    fireEvent.click(undoButton);
+    fireEvent.click(redoButton);
+
+    expect(screen.getByTestId('color-display')).toHaveStyle({
+      backgroundColor: '#FFFFFF'
+    });
   });
 });
